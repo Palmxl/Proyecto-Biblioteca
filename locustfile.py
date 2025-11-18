a45c2e2be78be51d4d0e7473c0bdc8d6805bfccc
Juan Martín Sánchez <140517935+Palmxl@users.noreply.github.com>
2025-11-17 23:03:04 -0500

    Refactor user data generation in locustfile

--- locustfile.py
+++ locustfile.py
@@ -1,32 +1,29 @@
 from locust import HttpUser, task, between
-import random, json
+import random
 
 class BibliotecaUser(HttpUser):
-    wait_time = between(1, 3)  # tiempo de espera entre tareas
+    wait_time = between(1, 3)
 
-    # --- Simular préstamo ---
     @task(3)
     def prestar_libro(self):
         data = {
-            "isbn": f"ISBN{random.randint(1,15):05d}",  # tus ISBN reales (1 a 15)
-            "user": f"usuario{random.randint(1, 200)}"
+            "isbn": f"ISBN{random.randint(1,15):05d}",
+            "user": f"usuario{random.randint(1,200)}"
         }
         self.client.post("/prestamo", json=data)
 
-    # --- Simular renovación ---
     @task(2)
     def renovar_libro(self):
         data = {
             "isbn": f"ISBN{random.randint(1,15):05d}",
-            "user": f"usuario{random.randint(1, 200)}"
+            "user": f"usuario{random.randint(1,200)}"
         }
         self.client.post("/renovacion", json=data)
 
-    # --- Simular devolución ---
     @task(1)
     def devolver_libro(self):
         data = {
             "isbn": f"ISBN{random.randint(1,15):05d}",
-            "user": f"usuario{random.randint(1, 200)}"
+            "user": f"usuario{random.randint(1,200)}"
         }
         self.client.post("/devolucion", json=data)
